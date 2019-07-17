3b7303b07356140942aa8fe5c4f1daff3bbfafe7
Michael Lai <mike24567@gmail.com>
2019-07-17 15:15:42 -0700

    first iteration of speech to text

--- speech2txt.py
+++ speech2txt.py
@@ -1,16 +1,31 @@
-import webbrowser
+import webbrowser as wb
 import speech_recognition as sr
 
 
-print('hello1')
+print('hello I am working')
 
 r1 = sr.Recognizer()
 r2 = sr.Recognizer()
 r3 = sr.Recognizer()
 
-# with sr.Microphone() as source:
-#     print('[search edureka: search youtube]')
-#     print('speak now')
-#      audio = r3.listen(source)
+with sr.Microphone() as source:
+    print('[search edureka: search youtube]')
+    print('speak now')
+    audio = r3.listen(source)
 
-    # if 'edureka' in r2.recognize_google(audio): 
+if 'video' in r1.recognize_google(audio):
+    r1 = sr.Recognizer()
+    url = 'https://www.youtube.com/results?search_query='
+    with sr.Microphone() as source:
+        print('search yfor a video')
+        audio = r1.listen(source)
+
+        try:
+            get = r1.recognize_google(audio)
+            print(get)
+            wb.get().open_new(url+get)
+        except sr.UnknownValueError:
+            print('could not understand')
+        except sr.RequestError as e:
+            print(e)
+        
